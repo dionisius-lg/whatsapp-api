bfccf04f2bcac063eded40fff73d3698f93e8781
dion <dion@jsm.co.id>
2025-07-09 09:57:23 +0700

    Refactor filterColumn and filterData functions for improved readability and performance

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,42 +1,43 @@
 /**
- * remove invalid column from object
- * @param {object} object 
- * @param {array} keys 
+ * Remove invalid column from object
+ * @param  {Object} object - Raw data column
+ * @param  {string[]} keys - Data column to be keep
+ * @returns {Object} - Filtered data
  */
 exports.filterColumn = (object = {}, keys = []) => {
-    Object.keys(object).forEach((key) => {
-        if (!keys.includes(key)) {
-            delete object[key];
-        }
-    });
+    const allowedKeys = new Set(keys); // use new Set(['array value']) for faster lookup (O(1))
+
+    return Object.fromEntries(Object.entries(object).filter(([key]) => allowedKeys.has(key)));
 };
 
 /**
- * remove invalid data from object
- * @param {object} object
+ * Remove invalid data from object
+ * @param  {Object} object - Raw data
+ * @returns {Object} - Filtered data
  */
 exports.filterData = (object = {}) => {
-    Object.keys(object).forEach((key) => {
-        if (object[key] === undefined || object[key] === false) {
-            delete object[key];
-        }
+    return Object.entries(object).reduce((acc, [key, value]) => {
+        if (value === undefined || value === false) {
+            return acc;
+        } // Skip invalid values
+		
+        // Convert empty strings (excluding null) to null
+        acc[key] = typeof value === 'string' && value.trim() === '' ? null : value;
 
-        if ((typeof object[key] === 'string' && (object[key]).trim() === '') && object[key] !== null) {
-            object[key] = null;
-        }
-    });
+        return acc;
+    }, {});
 };
 
 /**
- * return new valid object value
- * @param {object} object
- * @param {array} params
- * @returns {object} options
+ * Return new valid object value
+ * @param  {Object} object
+ * @param  {string[]} parameters
+ * @returns {Object}
  */
-exports.filterParam = (object = {}, params = []) => {
+exports.filterParam = (object = {}, parameters = []) => {
     let options = {};
 
-    params.forEach((param) => {
+    parameters.forEach(function (param) {
         if (object[param] !== undefined || object[param] !== '') {
             options[param] = object[param];
         }
