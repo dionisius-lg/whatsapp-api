b403de20c54f4d6cf076af5f0d5bbbf5df839362
dion <dion@jsm.co.id>
2025-08-05 12:08:10 +0700

    update messages controller

--- src/helpers/media.js
+++ src/helpers/media.js
@@ -1,8 +1,14 @@
 const axios = require('axios');
+const fs = require('fs');
+const dateFormat = require('dateformat');
 const mime = require('mime-types');
+const config = require('./../config');
+const logger = require('./logger');
 const valueHelper = require('./value');
 
-const { isEmpty } = valueHelper;
+const { createWriteStream, existsSync, mkdirSync, statSync } = fs;
+const { file_dir } = config;
+const { isEmpty, isValidUrl, randomString } = valueHelper;
 
 exports.getMimeType = async (url) => {
     try {
@@ -23,6 +29,61 @@ exports.getMimeType = async (url) => {
 
         return mimeType;
     } catch (_err) {
+        return null;
+    }
+};
+
+exports.downloadFile = async (url) => {
+    try {
+        if (isEmpty(url) || !isValidUrl(url)) {
+            throw new Error('Invalid url');
+        }
+
+        const ymd = dateFormat(new Date(), 'yyyy/mm/dd');
+        const path = `${file_dir}/${ymd}`;
+
+        if (!existsSync(path)) {
+            mkdirSync(path, { recursive: true, mode: 0o777 });
+        }
+
+        const api = await axios.get(url, { responseType: 'stream' });
+        const mimetype = api.headers['content-type'] || '';
+        const extension = mime.extension(mimetype);
+
+        let filename = api.headers['content-disposition'] || api.headers['content-dispositon'] || '';
+        filename = (filename.split('filename=')[1] || '').split('.')[0];
+        filename = filename.replace(/[^a-zA-Z0-9 ]/g, '').replace(/\s+/g, ' ').trim();
+
+        if (isEmpty(filename)) {
+            filename = randomString(24, true);
+        }
+
+        filename += `${Math.floor(1000 + Math.random() * 9000)}`;
+        filename += `.${extension}`;
+
+        const filestream = createWriteStream(`${path}/${filename}`);
+
+        api.data.pipe(filestream);
+
+        await new Promise((resolve, reject) => {
+            filestream.on('finish', resolve);
+            filestream.on('error', reject);
+        });
+
+        const filesize = statSync(`${path}/${filename}`).size;
+
+        return {
+            path,
+            filename,
+            size: filesize,
+            mimetype
+        };
+    } catch (err) {
+        logger.error({
+            from: 'media:downloadFile',
+            message: `Download media error! ${err?.message}`
+        });
+
         return null;
     }
 };
