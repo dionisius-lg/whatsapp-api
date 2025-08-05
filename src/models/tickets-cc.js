b2951b1b0863f683011ddecd77ed2d8405d0b850
dion <dion@jsm.co.id>
2025-08-05 09:36:38 +0700

    add custom conditions on tickets model, fix var name to ticketsModel

--- src/models/tickets-cc.js
+++ src/models/tickets-cc.js
@@ -2,17 +2,43 @@ const dbQueryHelper = require('./../helpers/db-query');
 const table = 'tickets';
 
 exports.getAll = async (conditions) => {
+    let customConditions = [];
+
+    if (conditions.except_ticket_status_id !== undefined) {
+        if (Array.isArray(conditions.except_ticket_status_id)) {
+            customConditions.push(`${table}.ticket_status_id NOT IN (${(conditions.except_ticket_status_id).join(',')})`);
+        } else {
+            customConditions.push(`${table}.ticket_status_id != '${conditions.except_ticket_status_id}'`);
+        }
+
+        delete conditions.except_ticket_status_id;
+	}
+
     return await dbQueryHelper.getAll({
         table,
         conditions,
+        customConditions,
         cc: 1
     });
 };
 
 exports.getDetail = async (conditions) => {
+    let customConditions = [];
+
+    if (conditions.except_ticket_status_id !== undefined) {
+        if (Array.isArray(conditions.except_ticket_status_id)) {
+            customConditions.push(`${table}.ticket_status_id NOT IN (${(conditions.except_ticket_status_id).join(',')})`);
+        } else {
+            customConditions.push(`${table}.ticket_status_id != '${conditions.except_ticket_status_id}'`);
+        }
+
+        delete conditions.except_ticket_status_id;
+	}
+
     return await dbQueryHelper.getDetail({
         table,
         conditions,
+        customConditions,
         cc: 1
     });
 };
